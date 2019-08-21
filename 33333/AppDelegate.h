51c012a1f1b539e17cec066b2ea30765aff16a7e
KOElight <792212278@qq.com>
2019-08-21 13:45:43 +0800

    Initial Commit

---
+++ 33333/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  33333
+//
+//  Created by koe on 2019/8/21.
+//  Copyright © 2019 koe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 33333/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  33333
+//
+//  Created by koe on 2019/8/21.
+//  Copyright © 2019 koe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
